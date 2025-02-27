93905f5a71003dff4b9c411455d4f16a2f3782b5
NetologyStudy <ownlove.y@rambler.ru>
2025-02-27 12:56:39 +0300

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -14,7 +14,7 @@ def setup_logger(log_file="scraper.log"):
         file_handler.setFormatter(formatter)
         logger.addHandler(file_handler)
 
-        stream_handler = logging.StreamHandler(sys.stdout)  # Use sys.stdout
+        stream_handler = logging.StreamHandler(sys.stdout)
         stream_handler.setFormatter(formatter)
         logger.addHandler(stream_handler)
 
