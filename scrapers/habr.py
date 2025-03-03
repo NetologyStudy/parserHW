980d9352fe9aaf5b8c17ffe6bf760e196837d622
practic <maks.mayer.ru@outlook.com>
2025-03-03 21:48:46 +0300

    Change commit

--- scrapers/habr.py
+++ scrapers/habr.py
@@ -6,20 +6,21 @@ from scrapers.check_word import contains_any_word, contains_any_word_reg
 
 
 def scrape_habr(logger):
+    session = requests.Session()
     url = "https://habr.com/ru/articles/"
     headers = Headers(browser='chrome', os='win').generate()
     key_words = ['дизайн', 'it', 'web', 'python']
     logger.info(f"Начинаем скрапинг {url}")
 
     try:
-        response = requests.get(url, headers=headers)
+        response = session.get(url, headers=headers)
         response.raise_for_status()
         soup = BeautifulSoup(response.text, features='lxml')
         articles_list = soup.select('article.tm-articles-list__item')
         parsed_data = []
         for article in articles_list:
             article_link = 'https://habr.com' + article.select_one('a.tm-title__link')['href']
-            response_article = requests.get(article_link)
+            response_article = session.get(article_link)
             soup = BeautifulSoup(response_article.text, features='lxml')
             article_title = soup.select_one('h1').text.strip()
             article_time = soup.select_one('time')['datetime']
