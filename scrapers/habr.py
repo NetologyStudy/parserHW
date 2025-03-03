e6aa4fd17c547e113adce1e7ac9df311466f9123
practic <maks.mayer.ru@outlook.com>
2025-03-03 21:56:15 +0300

    Change commit

--- scrapers/habr.py
+++ scrapers/habr.py
@@ -20,10 +20,8 @@ def scrape_habr(logger):
         parsed_data = []
         for article in articles_list:
             article_link = 'https://habr.com' + article.select_one('a.tm-title__link')['href']
-            response_article = session.get(article_link)
-            soup = BeautifulSoup(response_article.text, features='lxml')
-            article_title = soup.select_one('h1').text.strip()
-            article_time = soup.select_one('time')['datetime']
+            article_title = article.select_one('h2').text.strip()
+            article_time = article.select_one('time')['datetime']
             if contains_any_word_reg(article_title.lower(), key_words):
                 parsed_data.append({
                     'date': article_time,
--- utils/logger.py
+++ utils/logger.py
@@ -6,15 +6,12 @@ def setup_logger(log_file="scraper.log"):
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.INFO)
 
-    if not logger.handlers:
-        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-
-        file_handler = logging.FileHandler(log_file, encoding='utf-8')
-        file_handler.setFormatter(formatter)
-        logger.addHandler(file_handler)
-
-        stream_handler = logging.StreamHandler(sys.stdout)
-        stream_handler.setFormatter(formatter)
-        logger.addHandler(stream_handler)
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    file_handler = logging.FileHandler(log_file, encoding='utf-8')
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+    stream_handler = logging.StreamHandler(sys.stdout)
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
 
     return logger
